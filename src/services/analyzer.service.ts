3624fbf7446453c14e128b25809c6e31d9cf758b
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-11-15 01:37:54 +0700

    Improve analyzer scoring system

--- src/services/analyzer.service.ts
+++ src/services/analyzer.service.ts
@@ -41,12 +41,49 @@ class AnalyzerService {
     return array1.filter(value => !array2.includes(value));
   }
 
+  getTopThreeFromDictionary = (dictionary: { [id: string]: number }) => {
+    // Convert the dictionary into an array of key-value pairs
+    const itemsArray = Object.entries(dictionary);
+
+    // Sort the array based on the values in descending order
+    const sortedArray = itemsArray.sort((a, b) => b[1] - a[1]);
+
+    // Take the top 3 items
+    const top3 = sortedArray.slice(0, 3).map(([key, value]) => {
+      return {
+        name: key,
+        score: value
+      }
+    });
+    return top3
+  }
+
+  getBottomThreeFromDictionary = (dictionary: { [id: string]: number }) => {
+    // Convert the dictionary into an array of key-value pairs
+    const itemsArray = Object.entries(dictionary);
+
+    // Sort the array based on the values in descending order
+    const sortedArray = itemsArray.sort((a, b) => a[1] - b[1]);
+
+    // Take the top 3 items
+    const bottom3 = sortedArray.slice(0, 3).map(([key, value]) => {
+      return {
+        name: key,
+        score: value
+      }
+    });
+    return bottom3
+  }
+
   calculateScore = ({ request, ingredients }: { request: AnalysisRequest, ingredients: (Ingredient | null)[] }) => {
     let totalScore = 0
     let denominator = 0
 
     let posEffects: string[] = []
     let negEffects: string[] = []
+
+    let ingDepthAnalysis: { [id: string]: number } = {}
+    let skinTypesDepthAnalysis: { [id: string]: number } = {}
 
     ingredients.map((ing, index) => {
       if (!ing) return
@@ -79,13 +116,14 @@ class AnalyzerService {
       const skin_types_difference = this.getArrayDifference(skin_types_arr, request.skinTypes)
 
       const bad_for_skin_types_intersecting = this.getIntersection(bad_for_skin_types_arr, request.skinTypes)
+      const bad_for_skin_types_difference = this.getArrayDifference(bad_for_skin_types_arr, request.skinTypes)
 
       const currentPosBaseScore = BasePointValue.POSITIVE_BASE_POINT - (index / ingredients.length)
       const currentNegBaseScore = BasePointValue.NEGATIVE_BASE_POINT + (index / ingredients.length)
 
       const posScore = (good_for_intersecting.length * currentPosBaseScore) +
         (skin_types_intersecting.length * currentPosBaseScore)
-        // ((skin_types_intersecting.length > 1 ? 1 : skin_types_intersecting.length) * currentPosBaseScore);
+      // ((skin_types_intersecting.length > 1 ? 1 : skin_types_intersecting.length) * currentPosBaseScore);
       // (benefit_intersecting.length * currentPosBaseScore)
 
       const neutScore = (good_for_difference.length * BasePointValue.NEUTRAL_BASE_POINT) +
@@ -94,9 +132,43 @@ class AnalyzerService {
       // (benefit_difference.length * BasePointValue.NEUTRAL_BASE_POINT)
       // (concerns_difference.length * BasePointValue.NEUTRAL_BASE_POINT * -1) +
 
-      const negScore = (bad_for_intersecting.length * currentNegBaseScore) + 
+      const negScore = (bad_for_intersecting.length * currentNegBaseScore) +
         (bad_for_skin_types_intersecting.length * currentNegBaseScore)
       // (concerns_intersecting.length * currentNegBaseScore)
+
+      // Create In Depth Scoring
+      good_for_intersecting.map(x => {
+        if (!ingDepthAnalysis[x]) ingDepthAnalysis[x] = 0
+        ingDepthAnalysis[x] = ingDepthAnalysis[x] + currentPosBaseScore
+      })
+      bad_for_intersecting.map(x => {
+        if (!ingDepthAnalysis[x]) ingDepthAnalysis[x] = 0
+        ingDepthAnalysis[x] = ingDepthAnalysis[x] + currentNegBaseScore
+      })
+      good_for_difference.map(x => {
+        if (!ingDepthAnalysis[x]) ingDepthAnalysis[x] = 0
+        ingDepthAnalysis[x] = ingDepthAnalysis[x] + BasePointValue.NEUTRAL_BASE_POINT
+      })
+      bad_for_difference.map(x => {
+        if (!ingDepthAnalysis[x]) ingDepthAnalysis[x] = 0
+        ingDepthAnalysis[x] = ingDepthAnalysis[x] - BasePointValue.NEUTRAL_BASE_POINT
+      })
+      skin_types_intersecting.map(x => {
+        if (!skinTypesDepthAnalysis[x]) skinTypesDepthAnalysis[x] = 0
+        skinTypesDepthAnalysis[x] = skinTypesDepthAnalysis[x] + currentPosBaseScore
+      })
+      skin_types_difference.map(x => {
+        if (!skinTypesDepthAnalysis[x]) skinTypesDepthAnalysis[x] = 0
+        skinTypesDepthAnalysis[x] = skinTypesDepthAnalysis[x] + BasePointValue.NEUTRAL_BASE_POINT
+      })
+      bad_for_skin_types_intersecting.map(x => {
+        if (!skinTypesDepthAnalysis[x]) skinTypesDepthAnalysis[x] = 0
+        skinTypesDepthAnalysis[x] = skinTypesDepthAnalysis[x] + currentNegBaseScore
+      })
+      bad_for_skin_types_difference.map(x => {
+        if (!skinTypesDepthAnalysis[x]) skinTypesDepthAnalysis[x] = 0
+        skinTypesDepthAnalysis[x] = skinTypesDepthAnalysis[x] - BasePointValue.NEUTRAL_BASE_POINT
+      })
 
       totalScore += (posScore + neutScore + negScore)
 
@@ -109,9 +181,11 @@ class AnalyzerService {
       score: totalScore,
       denominator: denominator,
       posEffects,
-      negEffects
+      negEffects,
+      ingDepthAnalysis,
+      skinTypesDepthAnalysis
     }
-  } 
+  }
 
   sortIngredients = (ingredients: (Ingredient | null)[]) => {
     const res: Ingredient[] = []
@@ -127,16 +201,16 @@ class AnalyzerService {
     return res
   }
 
-  preprocessIngredientsString = (ingredientsString: string) : string[] => {
+  preprocessIngredientsString = (ingredientsString: string): string[] => {
     return ingredientsString.split(",").map(x => x.trim().toLowerCase())
   }
 
-  flattenArray = (array : any[]) : string[] => {
+  flattenArray = (array: any[]): string[] => {
     const flattened = array.map(x => x.split(",").map((y: string) => y.trim())).flat()
     return [...new Set(flattened)]
-  } 
+  }
 
-  getPercentage = (score: number, denominator: number) : number => {
+  getPercentage = (score: number, denominator: number): number => {
     const percentage = score / denominator * 100
     if (percentage > 100) return 100
     else if (percentage < 0) return 0
@@ -145,12 +219,12 @@ class AnalyzerService {
 
   getAnalysis = async (request: AnalysisRequest) => {
     try {
-      const { productId, skinTypes, concerns, ingredients : ingredientsString } = request
+      const { productId, skinTypes, concerns, ingredients: ingredientsString } = request
 
-      let ing_name_arr : string[] = []
+      let ing_name_arr: string[] = []
       let productData: Product | null = null
 
-      if(productId) {
+      if (productId) {
         productData = await productServiceInstance.getById(productId);
         if (!productData) throw new Error("Product not found")
 
@@ -160,7 +234,7 @@ class AnalyzerService {
       }
 
       // If no ingredients found
-      if(ing_name_arr.length < 1) throw new Error("Analyzer can't find any ingredients")
+      if (ing_name_arr.length < 1) throw new Error("Analyzer can't find any ingredients")
 
       const ingredients = await Promise.all(ing_name_arr.map(x => ingredientsServiceInstance.getByName(x)))
       if (ingredients.length < 1) throw new Error("Product does not have enough ingredients")
@@ -174,7 +248,7 @@ class AnalyzerService {
       const incompatibleIngredients = this.flattenArray(ingredients.map(x => x?.incompatible_ingredients).filter(x => x && x !== ""))
       const additionalDescriptions = this.flattenArray(ingredients.map(x => x?.additional_description).filter(x => x && x !== ""))
 
-      const { score, denominator, negEffects, posEffects } = this.calculateScore({ request, ingredients: ingredients_sorted })
+      const { score, denominator, negEffects, posEffects, ingDepthAnalysis, skinTypesDepthAnalysis } = this.calculateScore({ request, ingredients: ingredients_sorted })
 
       return {
         id: Date.now(),
@@ -186,12 +260,16 @@ class AnalyzerService {
         ingredients: ingredients_sorted,
         product: productData,
         similarProducts,
-        goodForSkinType,
-        badForSkinType,
-        goodFor,
-        badFor,
+        // goodForSkinType: this.countStringOccurrences(goodForSkinType),
+        // badForSkinType: this.countStringOccurrences(badForSkinType),
+        // goodFor,
+        // badFor,
         incompatibleIngredients,
-        additionalDescriptions
+        additionalDescriptions,
+        goodFor: this.getTopThreeFromDictionary(ingDepthAnalysis).map(x => x.name),
+        goodForSkinType: this.getTopThreeFromDictionary(skinTypesDepthAnalysis).map(x => x.name),
+        badFor: this.getBottomThreeFromDictionary(ingDepthAnalysis).map(x => x.name),
+        badForSkinType: this.getBottomThreeFromDictionary(skinTypesDepthAnalysis).map(x => x.name)
       }
     } catch (error) {
       throw error
